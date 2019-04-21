640af4e6dd2454f16698464c62034e06410b7307
lu long <510865496@qq.com>
2019-04-21 12:58:06 +0000

    完成注册功能

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 
 class UsersController extends Controller
@@ -22,5 +24,19 @@ class UsersController extends Controller
             'phone' => 'required|unique:users|max:20',
             'password' => 'required|confirmed|min:6'
         ]);
+
+        $user = User::create([
+            'string_id'     => substr(md5($request->phone . 'long-wo' . Carbon::now()->timestamp), 0, 16),
+            'name'          => substr($request->phone, 0, 3) .'****'. substr($request->phone, 7, 4),
+            'phone'         => $request->phone,
+            'password'      => bcrypt($request->password),
+            'avatar'        => url('/default/avatar.jpg'),
+        ]);
+        if (!$user)
+        {
+
+        }
+        session()->flash('success', '注册成功，欢迎！');
+        return redirect()->route('home');
     }
 }
