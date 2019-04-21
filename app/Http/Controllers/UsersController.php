eda7c47dfd4e3fc27a523816390f9bfa5b935007
lu long <510865496@qq.com>
2019-04-21 13:51:45 +0000

    用户中心

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -9,10 +9,18 @@ use Auth;
 
 class UsersController extends Controller
 {
+
+    public function __construct()
+    {
+        $this->middleware('auth', [
+            'except' => ['create', 'store']
+        ]);
+    }
+
     public function show()
     {
-        dump(Auth::user() instanceof User ? Auth::user()->toArray() : null);
-        return view('users.show');
+        $user = Auth::user();
+        return view('users.show', compact('user'));
     }
 
     public function create()
---
+++ resources/views/shared/_user_info.blade.php
@@ -0,0 +1,4 @@
+<a href="{{ route('home') }}">
+    <img src="{{ $user->avatar }}" alt="{{ $user->name }}" class="gravatar"/>
+</a>
+<h1>{{ $user->name }}</h1>
